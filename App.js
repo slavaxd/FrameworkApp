3f6e7a234be88c3921954c108cbfb81823c1adfe
slavadx <slavadx@inbox.ru>
2019-01-31 10:18:56 +0200

    Login page done(icons facebook and google not ready)

--- App.js
+++ App.js
@@ -3,7 +3,7 @@ import { Platform, StatusBar, StyleSheet, View } from 'react-native';
 import { AppLoading, Asset, Font, Icon } from 'expo';
 
 import AppNavigator from './navigation/AppNavigator';
-import Login from './components/LoginPage';
+import LoginPage from './components/LoginPage';
 
 export default class App extends React.Component {
   state = {
@@ -22,8 +22,9 @@ export default class App extends React.Component {
     } else {
       return (
         <View style={styles.container}>
-          {Platform.OS === 'ios' && <StatusBar barStyle="default" />}
-          <AppNavigator />
+          {/* {Platform.OS === 'ios' && <StatusBar barStyle="default" />} */}
+          {/* <AppNavigator /> */}
+          <LoginPage />
         </View>
       );
     }
--- components/LoginPage.js
+++ components/LoginPage.js
@@ -1,35 +1,91 @@
 import React, { Component } from 'react'
-import { Text, View, StyleSheet } from 'react-native'
+import { Text, View, StyleSheet, Image, ImageBackground, TouchableOpacity } from 'react-native'
+import LoginForm from './LoginForm'
 
+const styles = StyleSheet.create({
+    container: {
+        flex: 0.6,
+        alignItems: "center",
+        justifyContent: 'center'
+    },
+    buttonContainer:{
+        paddingVertical: 15,
+        paddingRight: 50,
+        paddingLeft: 50,
+    },
+    buttonText:{
+        color: '#fff',
+        textAlign: 'center',
+        fontWeight: '700'
+    },
+    authContainer:{
+        flex: 0.4,
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'space-around'
+    },
+    buttonAuth:{
+        flexDirection: 'row',
+        paddingVertical: 15,
+        paddingRight: 50,
+        paddingLeft: 50,
+        borderRadius: 10,
+        width: '80%',
+    },
+    buttonFacebook:{
+        backgroundColor: "#4E668E",
+    },
+    buttonGoogle:{
+        backgroundColor: "#E26358"
+    },
+    buttonTextAuth: {
+        color: '#fff',
+        textAlign: 'center',
+        fontWeight: '700'
+    },
+})
 
 export default class LoginPage extends Component {
   render() {
     return (
-        <View style={styles.container}>
-            <View style={styles.loginContainer}>
-                <Image resizeMode="contain" style={styles.logo} source={require('../assets/images/robot-dev.png')} />
+        <React.Fragment>
+            <View style={styles.container}>
+                <ImageBackground 
+                    source={require('../assets/images/login.png')} 
+                    style={{ width: '100%', height: '100%', flex: 1 }}
+                >
+                    <LoginForm />
+                </ImageBackground>
             </View>
-            <View style={styles.formContainer}>
-                <LoginForm />
-            </View>
-       </View>
+            <View style={styles.authContainer}>
+                <TouchableOpacity 
+                    style={[styles.buttonAuth, styles.buttonFacebook]}
+                    onPress={this.onButtonPress}
+                    activeOpacity={1}>
+                    <View style={{alignSelf: 'flex-start'}}>
+                        <Image 
+                            source={require('../assets/images/facebook.png')} 
+                            style={{ width: 30, height: 30, }}
+                        ></Image>
+                    </View>
+                    <Text  style={styles.buttonTextAuth}>Увійти через Facebook</Text>
+                </TouchableOpacity>
+                <TouchableOpacity 
+                    style={[styles.buttonAuth, styles.buttonGoogle]}
+                    onPress={this.onButtonPress}
+                    activeOpacity={1}>
+                    <Image 
+                        source={require('../assets/images/google.png')} 
+                        style={{ width: 30, height: 30,  }}
+                    ></Image>
+                    <Text  style={styles.buttonTextAuth}>Увійти через Google</Text>
+                </TouchableOpacity>  
+                <TouchableOpacity 
+                    onPress={this.onButtonPress}>
+                    <Text  style={{color: '#444'}}>Створити акаунт</Text>
+                </TouchableOpacity> 
+            </View>  
+        </React.Fragment>
     )
   }
 }
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#2c3e50',
-    },
-    loginContainer:{
-        alignItems: 'center',
-        flexGrow: 1,
-        justifyContent: 'center'
-    },
-    logo: {
-        position: 'absolute',
-        width: 300,
-        height: 100
-    }
-})
